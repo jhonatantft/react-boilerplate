fda7295886b32cf461d2e496c2db72d26f30b7b4
jhonatantft <jhonatan.tft@gmail.com>
2019-09-30 20:39:23 -0300

    add: sass style sheets

--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './hello-world.css';
+import main from './main.scss';
 
 const HelloWorld = ({ title }) => (
-  <div className={style['hello-world']}>{title}</div>
+  <div className={style['hello-world']}>
+    {title}
+    <div className={main.text}>
+      This is a ping text made with SASS
+      <div className={main.color}>And here blue</div>
+    </div>
+  </div>
 );
 
 HelloWorld.propTypes = {
